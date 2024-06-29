280ca8d3ca6f4900879dee6d81f112a4a3de50eb
uhidontkno <hi@rare1k.dev>
2024-06-29 16:21:36 -0400

    fix: im a retard

--- main/malcrow/tailwind.config.js
+++ main/malcrow/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import catppuccin from '@catppuccin/daisyui'
-module.exports = {
+export default {
   content: ["./src/**/*.{html,css,js}","./src/index.html"],
   theme: {
     extend: {},
