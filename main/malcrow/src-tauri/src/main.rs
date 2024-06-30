94b259ecc468f80f3021de91c097709b02a9e7e3
uhidontkno <hi@rare1k.dev>
2024-06-30 14:43:51 -0400

    feat: check if dummy.exe exists

--- main/malcrow/src-tauri/src/main.rs
+++ main/malcrow/src-tauri/src/main.rs
@@ -1,8 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 pub mod helper;
+use std::path::Path;
+
 use helper::*;
+use msgbox::{create,IconType};
 fn main() {
+  if !Path::new("dummy.exe").exists() {
+    let _ = msgbox::create("Fatal Error", "dummy.exe is missing, and Malcrow cannot be used without it. Exiting!", IconType::Error);
+    std::process::exit(1);
+}
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![_get_config,_save_config])
         .run(tauri::generate_context!())
