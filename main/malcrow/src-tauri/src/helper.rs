587496babf4b73dac42f8555c11cd87e63aa11b7
uhidontkno <hi@rare1k.dev>
2024-07-03 11:34:38 -0400

    feat: implement making registry keys

--- main/malcrow/src-tauri/src/helper.rs
+++ main/malcrow/src-tauri/src/helper.rs
@@ -1,6 +1,60 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+
+use std::ptr;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use winapi::shared::minwindef::HKEY;
+use winapi::um::winnt::REG_OPTION_NON_VOLATILE;
+use winapi::um::winreg::RegCreateKeyExW;
+use winapi::um::winnt::KEY_SET_VALUE;
+use winapi::um::winreg::RegSetValueExW;
+
+pub fn create_registry_key(
+    root_key: HKEY, 
+    root_key_path: &str, 
+    subkey_name: &str, 
+    value_name: &str, 
+    value_type: u32, 
+    value_data: &[u8],
+) -> Result<(), String> {
+    let full_path = format!("{}\\{}", root_key_path, subkey_name);
+
+    unsafe {
+        let mut hkey: HKEY = ptr::null_mut();
+        let result = RegCreateKeyExW(
+            root_key, 
+            OsStr::new(&full_path).encode_wide().chain(Some(0)).collect::<Vec<u16>>().as_ptr(),
+            0, 
+            ptr::null_mut(), 
+            REG_OPTION_NON_VOLATILE, 
+            KEY_SET_VALUE, 
+            ptr::null_mut(), 
+            &mut hkey, 
+            ptr::null_mut()
+        );
+
+        match result {
+            0 => {
+                let success = RegSetValueExW(
+                    hkey, 
+                    OsStr::new(value_name).encode_wide().chain(Some(0)).collect::<Vec<u16>>().as_ptr(),
+                    0, 
+                    value_type, 
+                    value_data.as_ptr() as *const _, // Cast to a const byte pointer
+                    value_data.len() as u32 // Convert the length of value_data to u32 for cbData
+                );
+                if success == 0 {
+                    Ok(())
+                } else {
+                    Err(format!("Failed to set value"))
+                }
+            },
+            err => Err(format!("Failed to open key: {}", err)),
+        }
+    }
+}
 pub fn appdata() -> Option<String> {
     if let Some(appdata_dir) = env::var_os("APPDATA") {
         if let Some(appdata_dir) = appdata_dir.to_str() {
